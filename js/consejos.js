46fc4085f95e04ef9514e141c17bcd52c406713e
Jorge1061 <jorge1061@users.noreply.github.com>
2021-08-17 10:09:13 -0600

    upload

--- js/consejos.js
+++ js/consejos.js
@@ -1,13 +1,17 @@
-var c = 0;
-var length = 0;
+var cP = 0;
+var cV = 0;
+var lP = 0;
+var lV = 0;
 
-function consejos(url) {
+function consejos(url, i, c) {
     $.ajax({
         url: url,
         success: function(response) {
-            if (length == 0)
-                for (length = 0; response[length + 1] != undefined; length++);
-            var content = $(".posts")[0];
+            if (lV == 0 && lV != lP)
+                for (lV = 0; response[lV + 1] != undefined; lV++);
+            if (lP == 0)
+                for (lP = 0; response[lP + 1] != undefined; lP++);
+            var content = $(".posts")[i];
             content.innerHTML = "<img class='galeria' src='" + response[c].img_src + "' alt='Imagen no encontrada'>";
 
         },
@@ -17,20 +21,45 @@ function consejos(url) {
     });
 }
 
+function back(element) {
+    if (element == "piano") {
+        if (cP == 0)
+            cP = lP;
+        else
+            cP--;
+        consejos('https://jorge1061.github.io/Proyecto/json/consejosPiano.json', 0, cP);
+    } else {
+        if (cV == 0)
+            cV = lV;
+        else
+            cV--;
+        consejos('https://jorge1061.github.io/Proyecto/json/consejosCanto.json', 1, cV);
+    }
+}
+
+function foward(element) {
+    if (element == "piano") {
+        if (cP == lP)
+            cP = 0;
+        else
+            cP++;
+        consejos('https://jorge1061.github.io/Proyecto/json/consejosPiano.json', 0, cP);
+    } else {
+        if (cV == lV)
+            cV = 0;
+        else
+            cV++;
+        consejos('https://jorge1061.github.io/Proyecto/json/consejosCanto.json', 1, cV);
+    }
+}
+
 $(function() {
-    consejos('https://jorge1061.github.io/Proyecto/json/consejosPiano.json');
-    $("#back").click(function() {
-        if (c == 0)
-            c = length;
-        else
-            c--;
-        consejos('https://jorge1061.github.io/Proyecto/json/consejosPiano.json');
+    consejos('https://jorge1061.github.io/Proyecto/json/consejosPiano.json', 0, cP);
+    consejos('https://jorge1061.github.io/Proyecto/json/consejosCanto.json', 1, cV);
+    $(".back").click(function() {
+        back($(this).attr("id"));
     });
-    $("#foward").click(function() {
-        if (c == length)
-            c = 0;
-        else
-            c++;
-        consejos('https://jorge1061.github.io/Proyecto/json/consejosPiano.json');
+    $(".foward").click(function() {
+        foward($(this).attr("id"));
     });
 })
